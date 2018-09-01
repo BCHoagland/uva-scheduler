cfef63f78f31b5f1a748ccf6b0beb959d1919859
Brady Hoagland <bhoggland@gmail.com>
2018-09-01 10:35:07 -0400

    now supports user input, but it's inconvenient to format the input rn

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-function Time(starts, ends, days, name) {
-  this.starts = starts;
-  this.ends = ends;
-  this.days = days;
-  this.name = name;
-}
+var classNames = [];
+var classTimes = [];
 
-var classNames = ['DSA 1', 'DSA 1 Lab', 'COA 1', 'COA 1 Lab', 'ENGR 1624', 'CHEM 1610', 'CHEM 1610 Disc.', 'CHEM 1611 Lab', 'APMA 2120'];
-var classTimes = [[new Time([12, 12, 12], [13, 13, 13], [1, 3, 5])],
-                  [new Time([18.5], [19.5], [3]), new Time([18.5], [19.5], [4])],
-                  [new Time([14, 14, 14], [15, 15, 15], [1, 3, 5])],
-                  [new Time([18.5], [19.5], [1]), new Time([18.5], [19.5], [2])],
-                  [new Time([15.5, 15.5], [16.5, 16.5], [2, 4]), new Time([14, 14], [15, 15], [2, 4]), new Time([14, 14], [15, 15], [1, 5]), new Time([8, 8], [9, 9], [2, 4])],
-                  [new Time([9, 9, 9], [10, 10, 10], [1, 3, 5]), new Time([14, 14], [15, 15], [2, 4]), new Time([15.5, 18.5], [16.5, 19.5], [2, 4])],
-                  [new Time([18], [19.5], [1]), new Time([19.5], [21], [1]), new Time([18], [19.5], [2])],
-                  [new Time([14], [17], [3]), new Time([14], [17], [1]), new Time([9], [12], [2]), new Time([14], [17], [2]), new Time([14], [17], [4]), new Time([14], [17], [5])],
-                  [new Time([11, 11, 13, 11], [12, 12, 14, 12], [1, 3, 4, 5]), new Time([13, 13, 8.5, 13], [14, 14, 9.5, 14], [1, 3, 4, 5])]];
-// var classNames = ['a', 'b', 'c'];
-// var classTimes = [[new Time([12, 12, 12], [13, 13, 13], [1, 3, 5])],
-//                   [new Time([8], [9], [3]), new Time([10], [11], [4])],
-//                   [new Time([10, 11, 11], [11, 12, 12], [1, 3, 5])]];
+var w = [];
 
 
 
@@ -40,6 +23,135 @@ var yOffset = 100;
 var visibleCombo = 0;
 
 var twelveHourTime = false;
+
+
+
+
+function Time(starts, ends, days, name) {
+  this.starts = starts;
+  this.ends = ends;
+  this.days = days;
+  this.name = name;
+}
+
+function getFormSubstringFromIndex(str, sub, index) {
+  var endIndex = str.indexOf("&", index);
+
+  //if another element exists in the serialized form, the end index is the '&' separating it from the current element
+  if (endIndex > -1) {
+    return str.substring(index, endIndex);
+  //if this is the last element, just return the current index to the end of the string
+  } else {
+    return str.substring(index);
+  }
+}
+
+function dayStrToNum(str) {
+  switch (str) {
+    case "M":
+      return 1;
+      break;                        //TODO: DO YOU EVEN NEED THE BREAKS????
+    case "T":
+      return 2;
+      break;
+    case "W":
+      return 3;
+      break;
+    case "R":
+      return 4;
+      break;
+    case "F":
+      return 5;
+      break;
+    default:
+      return 0;
+  }
+}
+
+$('form').submit(function(event) {
+  event.preventDefault();
+  clasNames = [];
+  classTimes = [];
+
+
+  var formData = $(this).serialize();
+
+  var nameIndex = formData.indexOf("name");
+  var daysIndex = formData.indexOf("days");
+  var timesIndex = formData.indexOf("times");
+
+  var baseIndex = 0;
+
+  var n = 0;
+
+  while (nameIndex > -1 && daysIndex > -1 && timesIndex > -1) {
+    var name = getFormSubstringFromIndex(formData, "name", nameIndex + 5);
+    var days = getFormSubstringFromIndex(formData, "days", daysIndex + 5);
+    var times = getFormSubstringFromIndex(formData, "times", timesIndex + 6);
+
+    var isNewClass = name == '' ? false : true;
+
+    var daysArr = [];
+    for (var i = 0; i < days.length; i += 2) {
+      daysArr.push(dayStrToNum(days.substring(i, i + 1)));
+    }
+
+    var startArr = [];
+    var endArr = [];
+    var startIndex = 0;
+    var hyphenIndex = times.indexOf('-');
+    var endIndex = times.indexOf('_');
+    while (hyphenIndex > -1) {
+      startArr.push(parseInt(times.substring(startIndex, hyphenIndex)));
+      if (endIndex > -1) {
+        endArr.push(parseInt(times.substring(hyphenIndex + 1, endIndex)));
+      } else {
+        endArr.push(parseInt(times.substring(hyphenIndex + 1)));
+      }
+
+      endIndex = endIndex == -1 ? times.length : endIndex;
+      startIndex = endIndex + 1;
+      hyphenIndex = times.indexOf('-', startIndex);
+      endIndex = times.indexOf('_', startIndex);
+    }
+
+    var entry = new Time(startArr, endArr, daysArr);
+    if (isNewClass) {
+      classNames.push(name);
+      classTimes.push([entry]);
+    } else {
+      classTimes[classTimes.length - 1].push(entry);
+    }
+
+    baseIndex = timesIndex + 5;
+    var nameIndex = formData.indexOf("name", baseIndex);
+    var daysIndex = formData.indexOf("days", baseIndex);
+    var timesIndex = formData.indexOf("times", baseIndex);
+  }
+
+  w = getWorkingSchedules(classNames, classTimes);
+  visibleCombo = 0;
+});
+
+
+
+
+// var classNames = ['DSA 1', 'DSA 1 Lab', 'COA 1', 'COA 1 Lab', 'ENGR 1624', 'CHEM 1610', 'CHEM 1610 Disc.', 'CHEM 1611 Lab', 'APMA 2120'];
+// var classTimes = [[new Time([12, 12, 12], [13, 13, 13], [1, 3, 5])],
+//                   [new Time([18.5], [19.5], [3]), new Time([18.5], [19.5], [4])],
+//                   [new Time([14, 14, 14], [15, 15, 15], [1, 3, 5])],
+//                   [new Time([18.5], [19.5], [1]), new Time([18.5], [19.5], [2])],
+//                   [new Time([15.5, 15.5], [16.5, 16.5], [2, 4]), new Time([14, 14], [15, 15], [2, 4]), new Time([14, 14], [15, 15], [1, 5]), new Time([8, 8], [9, 9], [2, 4])],
+//                   [new Time([9, 9, 9], [10, 10, 10], [1, 3, 5]), new Time([14, 14], [15, 15], [2, 4]), new Time([15.5, 18.5], [16.5, 19.5], [2, 4])],
+//                   [new Time([18], [19.5], [1]), new Time([19.5], [21], [1]), new Time([18], [19.5], [2])],
+//                   [new Time([14], [17], [3]), new Time([14], [17], [1]), new Time([9], [12], [2]), new Time([14], [17], [2]), new Time([14], [17], [4]), new Time([14], [17], [5])],
+//                   [new Time([11, 11, 13, 11], [12, 12, 14, 12], [1, 3, 4, 5]), new Time([13, 13, 8.5, 13], [14, 14, 9.5, 14], [1, 3, 4, 5])]];
+// var classNames = ['a', 'b', 'c'];
+// var classTimes = [[new Time([12, 12, 12], [13, 13, 13], [1, 3, 5])],
+//                   [new Time([8], [9], [3]), new Time([10], [11], [4])],
+//                   [new Time([10, 11, 11], [11, 12, 12], [1, 3, 5])]];
+//
+// console.log(classTimes);
 
 
 
@@ -211,5 +323,4 @@ function showNextCombo() {
   }
 }
 
-var w = getWorkingSchedules(classNames, classTimes);
-console.log(w);
+// console.log(w);
